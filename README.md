a439e39e088f30fe1a1dacbef663979d66b51b1e
Corey600 <fcx600@163.com>
2016-08-05 17:57:28 +0800

    improve the function

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ A Javascript module for
 to connect
 [Disconf](https://github.com/knightliao/disconf)
 service.
+
+### todo list
+
+[] 启动时从disconf服务远程获取配置并写入配置文件，以config模块要求的规则命名{full_hostname}.EXT
+[] 监控zookeeper节点，实时将远程配置文件修改同步到本地磁盘文件，提供远程配置修改事件供外部监听
+[] 建立临时zookeeper节点，上传配置至远程服务器进行校验
+[] 只在磁盘上更新配置文件，不修改config模块加载的内容，需要另外重新加载config模块以更新磁盘上配置文件的修改
+[] 可在pm2启动时监听配置文件夹，配置文件被修改时重启服务。以实现远程修改配置文件，服务自动重启
+
+
+配置： 自身的配置文件路径，下载的配置写入文件的路径
--- index.js
+++ index.js
@@ -4,42 +4,11 @@
 
 'use strict';
 
-var config = require('config');
-var path = require('path');
-var fs = require('fs');
+var DisconfClient = require('./lib/DisconfClient');
 
-function reloadConfig() {
-    //noinspection JSCheckFunctionSignatures
-    var dir = config.util.getEnv('NODE_CONFIG_DIR');
-    for (var key in require.cache) {
-        //noinspection JSUnfilteredForInLoop
-        var fileName = key;
-        //noinspection JSCheckFunctionSignatures
-        if (-1 === fileName.indexOf(dir)) {
-            continue;
-        }
-        delete require.cache[fileName];
-    }
-    delete require.cache[require.resolve('config')];
-    return require('config');
-}
-
-module.exports.init = function (option, cb) {
-    //noinspection JSCheckFunctionSignatures
-    var _option = {
-        path: config.util.getEnv('NODE_CONFIG_DIR'),
-        filename: 'disconf.properties',
-        autoReloadConfig: true
-    };
-    config.util.extendDeep(_option, option);
-
-    var _path = path.join(_option.path, _option.filename);
-    fs.readFile(_path, {}, function (err, data) {
-        console.log(data.toString());
-        cb();
-    })
-};
-
-module.exports.reloadConfig = function () {
-    return reloadConfig();
-};
+/**
+ * 导出
+ *
+ * @type {DisconfClient}
+ */
+module.exports = new DisconfClient();
